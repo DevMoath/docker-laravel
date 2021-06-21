c117a6af72a379233fda24ece90b8d7ad327c706
Moath <moath.alhajrii@gmail.com>
2021-06-21 13:32:06 +0300

    use echo instead of print

--- install.sh
+++ install.sh
@@ -13,28 +13,28 @@ if [[ -e docker-compose.yml || -e docker ]]; then
     exit 1
 fi
 
-print "${NEW_LINE}${GREEN}Download docker-compose.yml file${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Download docker-compose.yml file${NO_COLOR}${NEW_LINE}"
 
 curl -fsSL "${BASE_URL}/docker-compose.yml" > docker-compose.yml
 
-print "${NEW_LINE}${GREEN}Create docker/nginx directory${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Create docker/nginx directory${NO_COLOR}${NEW_LINE}"
 
 mkdir -p docker/nginx
 
-print "${NEW_LINE}${GREEN}Download default.conf file${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Download default.conf file${NO_COLOR}${NEW_LINE}"
 
 curl -fsSL "${BASE_URL}/docker/nginx/default.conf" > docker/nginx/default.conf
 
-print "${NEW_LINE}${GREEN}Download general.conf file${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Download general.conf file${NO_COLOR}${NEW_LINE}"
 
 curl -fsSL "${BASE_URL}/docker/nginx/general.conf" > docker/nginx/general.conf
 
-print "${NEW_LINE}${GREEN}Download nginx.conf file${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Download nginx.conf file${NO_COLOR}${NEW_LINE}"
 
 curl -fsSL "${BASE_URL}/docker/nginx/nginx.conf" > docker/nginx/nginx.conf
 
-print "${NEW_LINE}${GREEN}Download php_fastcgi.conf file${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Download php_fastcgi.conf file${NO_COLOR}${NEW_LINE}"
 
 curl -fsSL "${BASE_URL}/docker/nginx/php_fastcgi.conf" > docker/nginx/php_fastcgi.conf
 
-print "${NEW_LINE}${GREEN}Done${NO_COLOR}${NEW_LINE}"
+echo "${NEW_LINE}${GREEN}Done${NO_COLOR}${NEW_LINE}"
