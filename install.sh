447c3ba50b94cb3ac77359dd1204702e5c5f8e6b
Moath <moath.alhajrii@gmail.com>
2021-06-30 17:06:47 +0300

    change hashbang
    use echo -e instead of printf

--- install.sh
+++ install.sh
@@ -1,40 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 BASE_URL="https://raw.githubusercontent.com/DevMoath/docker-compose-laravel/master"
-GREEN="\e[32m"
-NO_COLOR="\e[0m"
-NEW_LINE="\n"
-RED='\033[0;31m'
+RED="\033[1;31m"
+GREEN="\033[1;32m"
 
 if [[ -e docker-compose.yml || -e docker ]]; then
-    printf "${NEW_LINE}${RED}it seems you have 'docker-compose.yml' file or 'docker' directory, make sure you delete/rename it${NO_COLOR}${NEW_LINE}"
+    echo -e "${RED}it seems you have 'docker-compose.yml' file or 'docker' directory, make sure you delete/rename it" >&2
     exit 1
 fi
 
-printf "${NEW_LINE}${GREEN}Download docker-compose.yml file${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Download docker-compose.yml file" >&2
 
 curl -fsSL "${BASE_URL}/docker-compose.yml" > docker-compose.yml
 
-printf "${NEW_LINE}${GREEN}Create docker/nginx directory${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Create docker/nginx directory" >&2
 
 mkdir -p docker/nginx
 
-printf "${NEW_LINE}${GREEN}Download default.conf file${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Download default.conf file" >&2
 
 curl -fsSL "${BASE_URL}/docker/nginx/default.conf" > docker/nginx/default.conf
 
-printf "${NEW_LINE}${GREEN}Download general.conf file${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Download general.conf file" >&2
 
 curl -fsSL "${BASE_URL}/docker/nginx/general.conf" > docker/nginx/general.conf
 
-printf "${NEW_LINE}${GREEN}Download nginx.conf file${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Download nginx.conf file" >&2
 
 curl -fsSL "${BASE_URL}/docker/nginx/nginx.conf" > docker/nginx/nginx.conf
 
-printf "${NEW_LINE}${GREEN}Download php_fastcgi.conf file${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Download php_fastcgi.conf file" >&2
 
 curl -fsSL "${BASE_URL}/docker/nginx/php_fastcgi.conf" > docker/nginx/php_fastcgi.conf
 
-printf "${NEW_LINE}${GREEN}Done${NO_COLOR}${NEW_LINE}"
+echo -e "${GREEN}Done" >&2
